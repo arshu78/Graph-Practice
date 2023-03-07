40ce94f67e69ff2ae3234347407eae33a6489ec2
Arshad Shirgave <97735833+arshu78@users.noreply.github.com>
2023-03-07 21:59:15 +0530

    Time: 49 ms (81.40%), Space: 21.4 MB (26.80%) - LeetHub

---
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
@@ -0,0 +1,88 @@
+
+class DisjointSet {
+    vector<int> rank, parent, size;
+public:
+    DisjointSet(int n) {
+        rank.resize(n + 1, 0);
+        parent.resize(n + 1);
+        size.resize(n + 1);
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+
+    int findUPar(int node) {
+        if (node == parent[node])
+            return node;
+        return parent[node] = findUPar(parent[node]);
+    }
+
+    void unionByRank(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (rank[ulp_u] < rank[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+        }
+        else if (rank[ulp_v] < rank[ulp_u]) {
+            parent[ulp_v] = ulp_u;
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            rank[ulp_u]++;
+        }
+    }
+
+    void unionBySize(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (size[ulp_u] < size[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+};
+class Solution {
+public:
+    int removeStones(vector<vector<int>>& stones) { 
+        int n=stones.size();
+        int maxRow=0,maxCol=0;
+        
+        for(auto it: stones)
+        {
+            maxRow=max(maxRow,it[0]);
+            maxCol=max(maxCol,it[1]);
+            
+        }
+        
+        // Initialize DisjointSet with maximum row and col
+        
+        DisjointSet ds(maxRow+maxCol+1);
+        unordered_map<int,int> mp;
+        for(auto it:stones)
+        {
+            int nrow=it[0];
+            int ncol= it[1] +maxRow+1;
+            
+            ds.unionBySize(nrow,ncol);
+            mp[nrow]=1;
+            mp[ncol]=1;
+        }
+        
+        int ans=0;
+        for(auto it: mp)
+        {
+            if(ds.findUPar(it.first)==it.first)
+            {
+                ans++;
+            }
+        }
+        return n-ans;
+    }
+};
